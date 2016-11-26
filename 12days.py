e9b2561da21258a09b391bc8746d8408c29481a4
Thomas Denney <me@thomasdenney.co.uk>
2016-11-26 19:36:50 +0000

    Swap files round

--- 12days.py
+++ 12days.py
@@ -1,10 +1,28 @@
-l=["Twelve Drummers Drumming","Eleven Pipers Piping","Ten Lords a Leaping","Nine Ladies Dancing","Eight Maids a Milking","Seven Swans a Swimming","Six Geese a Laying","Five Golden Rings","Four Calling Birds","Three French Hens","Two Turtle Doves","partridge in a Pear Tree"]
-n=["first","second","third","fourth","fifth","sixth","seventh","eighth","ninth","tenth","eleventh","twelfth"]
+# This is a code golfing challenge. The goal is to write a program equivalent to
+# this one, with the smallest number of bytes in the source file
+# You can use any programming language you like
+lyrics = [
+  "Twelve Drummers Drumming",
+  "Eleven Pipers Piping",
+  "Ten Lords a Leaping",
+  "Nine Ladies Dancing",
+  "Eight Maids a Milking",
+  "Seven Swans a Swimming",
+  "Six Geese a Laying",
+  "Five Golden Rings",
+  "Four Calling Birds",
+  "Three French Hens",
+  "Two Turtle Doves",
+  "partridge in a Pear Tree" ]
+
+nums = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh",
+        "eighth", "ninth", "tenth", "eleventh", "twelfth"]
+
 for i in range(0,12):
- print("On the "+nums[i]+" day of Christmas")
- print("my true love sent to me:")
- for j in range(0,i+1):
-  l=lyrics[11-i+j]
-  if j==i:print(("And a " if i>1 else "A ")+l)
-  else:print(l)
- if i!=11:print("")
+  print("On the " + nums[i] + " day of Christmas")
+  print("my true love sent to me:")
+  for j in range(0, i + 1):
+    l = lyrics[11 - i + j]
+    if j == i: print(("And a " if i > 1 else "A ") + l)
+    else: print(l)
+  if i != 11: print("")
